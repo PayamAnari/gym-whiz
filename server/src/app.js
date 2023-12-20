4b52d56ae8fd652d3da5978192039c7909e1fca4
PayamAnari <anari.p62@gmail.com>
2023-12-20 15:30:21 +0100

    Add user route

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+// Create an express server
+const app = express();
+
+// Tell express to use the json middleware
+app.use(express.json());
+// Allow everyone to access our API. In a real application, we would need to restrict this!
+app.use(cors());
+
+/****** Attach routes ******/
+/**
+ * We use /api/ at the start of every route!
+ * As we also host our client code on heroku we want to separate the API endpoints.
+ */
+
+export default app;
