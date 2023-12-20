ad232a95b9ba47c52e96c4bae05139c6a5e5ac20
PayamAnari <anari.p62@gmail.com>
2023-12-20 20:36:54 +0100

    Add get users

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -4,3 +4,15 @@ import validationErrorMessage from '../util/validationErrorMessage.js';
 import bcrypt from 'bcrypt';
 import multer from 'multer';
 import { v2 as cloudinary } from 'cloudinary';
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, result: users });
+  } catch (error) {
+    logError(error);
+    res
+      .status(500)
+      .json({ success: false, msg: 'Unable to get users, try again later' });
+  }
+};
