e2d66cebbba8826a0eb06c2052d88b00050a7e91
PayamAnari <anari.p62@gmail.com>
2023-12-28 12:32:12 +0100

    modify home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,33 @@
 import React from "react";
-
+import Header from "../../components/Header/Header";
 import TEST_ID from "./Home.testid";
+import WhyGymWhiz from "../../components/WhyGymWhiz/WhyGymWhiz";
+import Subscribe from "../../components/Subscribe/Subscribe";
+import GymList from "../../components/GymList/GymList";
+import Advertisement from "../../components/Advertisement/Advertisement";
+import Bmi from "../../components/Bmi/Bmi";
+import OurStory from "../../components/OurStory/OurStory";
+import "./Home.css";
+import { useGyms } from "../../contexts/GymContext";
+import Loading from "../../components/Loading/Loading";
 
 const Home = () => {
+  const { isLoading } = useGyms();
   return (
     <div data-testid={TEST_ID.container}>
-      <h1>This is the homepage</h1>
-      <p>Good luck with the project!</p>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Header />
+          <WhyGymWhiz />
+          <Subscribe />
+          <GymList isHomePage={true} />
+          <Bmi />
+          <Advertisement />
+          <OurStory />
+        </>
+      )}
     </div>
   );
 };
